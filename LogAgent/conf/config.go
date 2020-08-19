ec21a98cf4f752212803657e4b09f2a8d184a6ba
micor5688 <micor5688@163.com>
2020-08-19 22:06:35 +0800

    修改代码布局

---
+++ LogAgent/conf/config.go
@@ -0,0 +1,18 @@
+package conf
+
+type AppConf struct {
+	KafkaConf `ini:"kafka"`
+	EtcdConf  `ini:"etcd"`
+}
+
+type KafkaConf struct {
+	Address     string `ini:"address"`
+	Topic       string `ini:"topic"`
+	ChanMaxSize int    `ini:"chan_max_size"`
+}
+
+type EtcdConf struct {
+	Address string `ini:"address"`
+	Timeout int    `ini:"timeout"`
+	Key     string `ini:"collect_log_key"`
+}
---
+++ LogAgent/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"LogCollection/LogAgent/conf"
+	"LogCollection/LogAgent/etcd"
+	"LogCollection/LogAgent/kafka"
+	"LogCollection/LogAgent/taillog"
+	"fmt"
+	"time"
+
+	"gopkg.in/ini.v1"
+)
+
+var (
+	cfg = new(conf.AppConf)
+)
+
+// func run() {
+// 	for {
+// 		select {
+// 		case line := <-taillog.ReadChan():
+// 			kafka.SendToKafka(cfg.KafkaConf.Topic, line.Text)
+// 		default:
+// 			time.Sleep(time.Millisecond * 500)
+// 		}
+// 	}
+// }
+
+func main() {
+	// 0.加载配置文件
+	err := ini.MapTo(cfg, "conf/config.ini")
+	if err != nil {
+		fmt.Printf("load conf failed, err:%v\n", err)
+		return
+	}
+	// 1.初始化kafka
+	err = kafka.Init([]string{cfg.KafkaConf.Address}, cfg.KafkaConf.ChanMaxSize)
+	if err != nil {
+		fmt.Printf("init kafka failed ,err:%v\n", err)
+		return
+	}
+	fmt.Println("init kafka success")
+	// // 2.初始化taillog
+	// err = taillog.Init(cfg.TailConf.Filename)
+	// if err != nil {
+	// 	fmt.Printf("init taillog failed, err:%v\n", err)
+	// 	return
+	// }
+	// fmt.Println("init taillog success")
+
+	// 2. 初始化etcd
+	err = etcd.Init(cfg.EtcdConf.Address, time.Duration(cfg.EtcdConf.Timeout)*time.Second)
+	if err != nil {
+		fmt.Printf("init etcd failed, err:%v\n", err)
+		return
+	}
+	fmt.Println("init etcd success.")
+
+	// 2.1 从etcd中获取日志收集项的配置信息
+	logEntryConf, err := etcd.GetConf(cfg.EtcdConf.Key)
+	if err != nil {
+		fmt.Printf("get conf from etcd failed,err:%v\n", err)
+		return
+	}
+	fmt.Printf("get conf from etcd success, %v\n", logEntryConf)
+	for index, value := range logEntryConf {
+		fmt.Printf("index:%v value:%v\n", index, value)
+	}
+
+	// 3. 收集日志发往Kafka
+	taillog.Init(logEntryConf)
+}
